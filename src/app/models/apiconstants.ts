63ddcc2c429571d0c54b9741ae3b450c1fbb5e15
Ajjay <akumar@nisum.com>
2017-08-28 19:31:26 -0700

    updating uri

--- src/app/models/apiconstants.ts
+++ src/app/models/apiconstants.ts
@@ -1,6 +1,6 @@
 export class Apiconstants {
     public static API_ENDPOINT='https://quickout.mynisum.com/quickout/api/cart?accountCode=10001';
-    public static APP_ENDPOINT='http://dev.pricepong.com:7777';
+    public static APP_ENDPOINT=https://akumar-nisum-com.github.io/demolite/';
     public static CART_ENDPOINT='https://quickout.mynisum.com/1?accountCode=10001&cartId=';
 }
  
